77c91f7dc69da678f33744fa35e43b49f8ac87c1
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-31 15:19:56 -0700

    added User_id to bookmarks so we have a bokkmarks method. added has_many bookmarks to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531220415) do
+ActiveRecord::Schema.define(version: 20160531221837) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
